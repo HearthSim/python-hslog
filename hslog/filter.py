770f98c547bb2ea8f3a9a380022baae78037048d
Julian Graham <julian@hearthsim.net>
2021-03-31 00:42:48 -0400

    Initial refactoring in preparation for release

--- hslog/filter.py
+++ hslog/filter.py
@@ -75,6 +75,15 @@ class BattlegroundsLogFilter:
         else:
             self._flushed_lines.append(line)
 
+    def _end_buffer(self):
+        if self._current_buffer:
+            if self._current_buffer.parent is not None:
+                self._current_buffer = self._current_buffer.parent
+            else:
+                buffer = self._current_buffer
+                self._current_buffer = None
+                self._flush_buffered_packets(buffer, should_skip=buffer.should_skip)
+
     def _flush_buffered_packets(self, buffer: Buffer, should_skip: bool = False):
         for buffered_item in buffer.buffer:
             if isinstance(buffered_item, Buffer):
@@ -86,44 +95,6 @@ class BattlegroundsLogFilter:
                         self._flushed_lines.append("X: " + buffered_item)
                 else:
                     self._flushed_lines.append(buffered_item)
-
-    def _parse_initial_tag(self, data):
-        sre = tokens.TAG_VALUE_RE.match(data)
-        if not sre:
-            raise RegexParsingError(data)
-        tag, value = sre.groups()
-        return parse_tag(tag, value)
-
-    def _start_new_buffer(self, buffer_type: str, subtype: str):
-        new_buffer = Buffer(buffer_type, subtype, parent=self._current_buffer)
-
-        if self._current_buffer:
-            self._current_buffer.buffer.append(new_buffer)
-
-        self._current_buffer = new_buffer
-
-    def _end_buffer(self):
-        if self._current_buffer:
-            if self._current_buffer.parent is not None:
-                self._current_buffer = self._current_buffer.parent
-            else:
-                buffer = self._current_buffer
-                self._current_buffer = None
-                self._flush_buffered_packets(buffer, should_skip=buffer.should_skip)
-
-    def _is_hero(self, card_id):
-        return card_id and "HERO" in card_id
-
-    def _get_card_id(self, entity: str):
-        card_id_index = entity.find("cardId=")
-        if card_id_index < 0:
-            return None
-
-        card_id_index += 7
-        following_space_index = entity.find(" ", card_id_index)
-        return entity[card_id_index:following_space_index] \
-            if following_space_index > card_id_index \
-            else entity[card_id_index:]
 
     def _get_block_type_and_card_id(self, opcode, data):
         _index = None
@@ -158,10 +129,178 @@ class BattlegroundsLogFilter:
 
         return block_type, self._get_card_id(entity)
 
-    def _get_tag_change_tag_and_value(self, data):
+    @staticmethod
+    def _get_card_id(entity: str):
+        card_id_index = entity.find("cardId=")
+        if card_id_index < 0:
+            return None
+
+        card_id_index += 7
+        following_space_index = entity.find(" ", card_id_index)
+        return entity[card_id_index:following_space_index] \
+            if following_space_index > card_id_index \
+            else entity[card_id_index:]
+
+    @staticmethod
+    def _get_tag_change_tag_and_value(data):
         sre = tokens.TAG_CHANGE_RE.match(data)
         _e, tag, value, _def_change = sre.groups()
         return tag, value
+
+    def _handle_block_end(self, line: str):
+        if self._buffering_entity():
+            self._end_buffer()
+
+        self._emit_line(line)
+
+        if self._current_buffer:
+            self._end_buffer()
+
+    def _handle_block_start(self, msg: str, line: str):
+        if self._buffering_entity():
+            self._end_buffer()
+
+        block_type, card_id = self._get_block_type_and_card_id("BLOCK_START", msg)
+
+        if self._current_buffer:
+            if self._current_buffer.subtype == "ATTACK":
+                if block_type == "TRIGGER" and card_id != "TB_BaconShop_8P_PlayerE":
+                    self._current_buffer.should_skip = False
+            elif self._current_buffer.subtype == "DEATHS":
+                self._current_buffer.should_skip = False
+
+        if block_type == "ATTACK":
+            if self._is_hero(card_id):
+                self._preserve_block_counter = 4
+            else:
+                self._start_new_buffer("BLOCK", block_type)
+                self._current_buffer.should_skip = True
+        elif block_type == "DEATHS":
+            self._start_new_buffer("BLOCK", block_type)
+            self._current_buffer.should_skip = True
+        elif block_type == "TRIGGER":
+            if card_id == "TB_BaconShop_8P_PlayerE":
+                self._start_new_buffer("BLOCK", block_type)
+
+        if self._preserve_block_counter > 0:
+            self._preserve_block_counter -= 1
+            if self._current_buffer:
+                self._current_buffer.should_skip = False
+
+        self._emit_line(line)
+
+    def _handle_entity(self, opcode: str, line: str):
+        if self._buffering_entity():
+            self._end_buffer()
+
+        self._start_new_buffer(opcode, "")
+        self._emit_line(line)
+
+    def _handle_entity_tag(self, msg: str, line: str):
+        tag, value = self._parse_initial_tag(msg)
+
+        if self._buffering_entity():
+            if self._current_buffer.buffer_type == "FULL_ENTITY":
+                if tag in BLACKLISTED_FULL_ENTITY_TAGS:
+                    self._start_new_buffer("__ENTITY_TAG", "")
+                    self._current_buffer.should_skip = True
+                    self._emit_line(line)
+                    self._end_buffer()
+                else:
+                    self._emit_line(line)
+            elif self._current_buffer.buffer_type == "SHOW_ENTITY":
+                if (
+                        tag in BLACKLISTED_FULL_ENTITY_TAGS or
+                        tag in BLACKLISTED_SHOW_ENTITY_TAGS
+                ):
+                    self._start_new_buffer("__ENTITY_TAG", "")
+                    self._current_buffer.should_skip = True
+                    self._emit_line(line)
+                    self._end_buffer()
+                else:
+                    self._emit_line(line)
+            else:
+                self._emit_line(line)
+        else:
+            self._emit_line(line)
+
+    def _handle_tag_change(self, msg: str, line: str):
+        if self._buffering_entity():
+            self._end_buffer()
+
+        tag, value = self._get_tag_change_tag_and_value(msg)
+
+        if (
+                self._current_buffer and
+                self._current_buffer.subtype == "TRIGGER" and
+                tag == "BOARD_VISUAL_STATE"
+        ):
+            self._current_buffer.buffer.append(line)
+
+            if value == "1" or value == "2":
+                for i in range(len(self._current_buffer.buffer)):
+                    buffered_item = self._current_buffer.buffer[i]
+                    if (
+                            isinstance(buffered_item, str) and
+                            "TAG_CHANGE" in buffered_item
+                    ):
+                        buf = Buffer("TAG_CHANGE", "", parent=self._current_buffer)
+                        buf.buffer.append(buffered_item)
+                        buf.should_skip = True
+                        self._current_buffer.buffer[i] = buf
+
+                if value == "2":
+                    setattr(self._current_buffer, "skip_tag_changes", True)
+
+                return
+
+        if (
+                tag.isdigit() or
+                tag in BLACKLISTED_TAGS or
+                (
+                        self._current_buffer is not None and
+                        hasattr(self._current_buffer, "skip_tag_changes")
+                )
+        ):
+            self._start_new_buffer("TAG_CHANGE", "")
+            self._current_buffer.should_skip = True
+            self._emit_line(line)
+            self._end_buffer()
+        else:
+            self._emit_line(line)
+
+    def _handle_options(self, level: str, ts: str, msg: str, line: str):
+        if msg.startswith("id="):
+            block_end = f"{level} {ts} GameState.DebugPrintPower() - BLOCK_END\n"
+            self._emit_line(block_end)
+            self._end_buffer()
+
+        if self._show_suppressed_lines:
+            suppressed_line = "X: " + line
+            if self._flushed_lines:
+                self._flushed_lines.append(suppressed_line)
+            else:
+                return suppressed_line
+
+    @staticmethod
+    def _is_hero(card_id):
+        return card_id and "HERO" in card_id
+
+    @staticmethod
+    def _parse_initial_tag(data):
+        sre = tokens.TAG_VALUE_RE.match(data)
+        if not sre:
+            raise RegexParsingError(data)
+        tag, value = sre.groups()
+        return parse_tag(tag, value)
+
+    def _start_new_buffer(self, buffer_type: str, subtype: str):
+        new_buffer = Buffer(buffer_type, subtype, parent=self._current_buffer)
+
+        if self._current_buffer:
+            self._current_buffer.buffer.append(new_buffer)
+
+        self._current_buffer = new_buffer
 
     def __iter__(self):
         return self
@@ -196,140 +335,19 @@ class BattlegroundsLogFilter:
                 opcode = msg.split()[0]
 
                 if opcode == "BLOCK_START":
-                    if self._buffering_entity():
-                        self._end_buffer()
-
-                    type, card_id = self._get_block_type_and_card_id(opcode, msg)
-
-                    if self._current_buffer:
-                        if self._current_buffer.subtype == "ATTACK":
-                            if type == "TRIGGER" and card_id != "TB_BaconShop_8P_PlayerE":
-                                self._current_buffer.should_skip = False
-                        elif self._current_buffer.subtype == "DEATHS":
-                            self._current_buffer.should_skip = False
-
-                    if type == "ATTACK":
-                        if self._is_hero(card_id):
-                            self._preserve_block_counter = 4
-                        else:
-                            self._start_new_buffer("BLOCK", type)
-                            self._current_buffer.should_skip = True
-                    elif type == "DEATHS":
-                        self._start_new_buffer("BLOCK", type)
-                        self._current_buffer.should_skip = True
-                    elif type == "TRIGGER":
-                        if card_id == "TB_BaconShop_8P_PlayerE":
-                            self._start_new_buffer("BLOCK", type)
-
-                    if self._preserve_block_counter > 0:
-                        self._preserve_block_counter -= 1
-                        if self._current_buffer:
-                            self._current_buffer.should_skip = False
-
-                    self._emit_line(line)
-
+                    self._handle_block_start(msg, line)
                 elif opcode == "BLOCK_END":
-                    if self._buffering_entity():
-                        self._end_buffer()
-
-                    self._emit_line(line)
-
-                    if self._current_buffer:
-                        self._end_buffer()
-
+                    self._handle_block_end(line)
                 elif opcode in ["FULL_ENTITY", "SHOW_ENTITY"]:
-                    if self._buffering_entity():
-                        self._end_buffer()
-
-                    self._start_new_buffer(opcode, "")
-                    self._emit_line(line)
-
+                    self._handle_entity(opcode, line)
                 elif opcode.startswith("tag="):
-                    tag, value = self._parse_initial_tag(msg)
-
-                    if self._buffering_entity():
-                        if self._current_buffer.buffer_type == "FULL_ENTITY":
-                            if tag in BLACKLISTED_FULL_ENTITY_TAGS:
-                                self._start_new_buffer("__ENTITY_TAG", "")
-                                self._current_buffer.should_skip = True
-                                self._emit_line(line)
-                                self._end_buffer()
-                            else:
-                                self._emit_line(line)
-                        elif self._current_buffer.buffer_type == "SHOW_ENTITY":
-                            if (
-                                    tag in BLACKLISTED_FULL_ENTITY_TAGS or
-                                    tag in BLACKLISTED_SHOW_ENTITY_TAGS
-                            ):
-                                self._start_new_buffer("__ENTITY_TAG", "")
-                                self._current_buffer.should_skip = True
-                                self._emit_line(line)
-                                self._end_buffer()
-                            else:
-                                self._emit_line(line)
-                        else:
-                            self._emit_line(line)
-                    else:
-                        self._emit_line(line)
-
+                    self._handle_entity_tag(msg, line)
                 elif opcode == "TAG_CHANGE":
-                    if self._buffering_entity():
-                        self._end_buffer()
-
-                    tag, value = self._get_tag_change_tag_and_value(msg)
-
-                    if (
-                            self._current_buffer and
-                            self._current_buffer.subtype == "TRIGGER" and
-                            tag == "BOARD_VISUAL_STATE"
-                    ):
-                        self._current_buffer.buffer.append(line)
-
-                        if value == "1" or value == "2":
-                            for i in range(len(self._current_buffer.buffer)):
-                                buffered_item = self._current_buffer.buffer[i]
-                                if (
-                                        isinstance(buffered_item, str) and
-                                        "TAG_CHANGE" in buffered_item
-                                ):
-                                    buf = Buffer("TAG_CHANGE", "", parent=self._current_buffer)
-                                    buf.buffer.append(buffered_item)
-                                    buf.should_skip = True
-                                    self._current_buffer.buffer[i] = buf
-
-                            if value == "2":
-                                setattr(self._current_buffer, "skip_tag_changes", True)
-
-                            continue
-
-                    if (
-                            tag.isdigit() or
-                            tag in BLACKLISTED_TAGS or
-                            (
-                                    self._current_buffer is not None and
-                                    hasattr(self._current_buffer, "skip_tag_changes")
-                            )
-                    ):
-                        self._start_new_buffer("TAG_CHANGE", "")
-                        self._current_buffer.should_skip = True
-                        self._emit_line(line)
-                        self._end_buffer()
-                    else:
-                        self._emit_line(line)
-
+                    self._handle_tag_change(msg, line)
                 else:
                     self._emit_line(line)
 
             elif method == "GameState.DebugPrintOptions":
-                if msg.startswith("id="):
-                    block_end = f"{level} {ts} GameState.DebugPrintPower() - BLOCK_END\n"
-                    self._emit_line(block_end)
-
-                if self._show_suppressed_lines:
-                    suppressed_line = "X: " + line
-                    if self._flushed_lines:
-                        self._flushed_lines.append(suppressed_line)
-                    else:
-                        return suppressed_line
+                self._handle_options(level, ts, msg, line)
             else:
                 self._emit_line(line)
