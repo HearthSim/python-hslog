96fc66aa599cc1547c5d28924c019bd635633bc5
Julian Graham <julian@hearthsim.net>
2023-10-06 19:25:49 -0400

    chore: fix comment doc formatting

--- hslog/exceptions.py
+++ hslog/exceptions.py
@@ -1,12 +1,8 @@
-"""
-Log parsing exceptions
-"""
+"""Log parsing exceptions"""
 
 
 class ParsingError(Exception):
-	"""
-	Generic exception that happens during log parsing.
-	"""
+	"""Generic exception that happens during log parsing."""
 	pass
 
 
@@ -15,21 +11,15 @@ class RegexParsingError(ParsingError):
 
 
 class CorruptLogError(ParsingError):
-	"""
-	Raised when a log is obviously corrupt, for example when containing NUL (0x00) bytes.
-	"""
+	"""Raised when a log is obviously corrupt, e.g. when containing NUL (0x00) bytes."""
 	pass
 
 
 class ExporterError(Exception):
-	"""
-	Generic exception that happens during PacketTree export.
-	"""
+	"""Generic exception that happens during PacketTree export."""
 	pass
 
 
 class MissingPlayerData(RuntimeError):
-	"""
-	Raised when it is not possible to reverse a LazyPlayer instance.
-	"""
+	"""Raised when it is not possible to reverse a LazyPlayer instance."""
 	pass
