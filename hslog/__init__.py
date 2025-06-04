2f9faa0f8e4e8754256d59014933163dee2bcad3
Benedict Etzel <benedict@hearthsim.net>
2025-06-04 14:46:29 +0200

    feat: drop dependency on pkg_resources

--- hslog/__init__.py
+++ hslog/__init__.py
@@ -1,6 +1,11 @@
-import pkg_resources
-
 from .parser import LogParser  # noqa
 
 
-__version__ = pkg_resources.require("hslog")[0].version
+try:
+	from importlib.metadata import version
+
+	__version__ = version("hslog")
+except ImportError:
+	import pkg_resources
+
+	__version__ = pkg_resources.require("hslog")[0].version
